cafb4af96f376c1ac8ee188ee10eaa7b6bdfe310
Christopher Cervantes <113720575+ccervantes24@users.noreply.github.com>
2022-09-16 19:20:02 +0000

    removing, appending, and popping

--- main.py
+++ main.py
@@ -4,19 +4,24 @@
 ##############################################################################################################
 # basic review
 # Find and display on the screen which character occupies the fifth position within the following word:
-"computer"
+# computer = "computer"
+# print(computer[-1])
+# # Find and display the index of the last occurrence of the word "practice" in the following sentence:
+# "In theory, theory and practice are the same. In practice, they are not."
 
-# Find and display the index of the last occurrence of the word "practice" in the following sentence:
-"In theory, theory and practice are the same. In practice, they are not."
-
-###############################################Review############################################################
+# print(newSentence ###############################################Review############################################################
 #####################################challenge 1################################################################
 # Create a list with 5 elements, inside the variable my_list. You can include strings, booleans, numbers, etc.
 
 # Add the element "motorcycle" to the following list of means of transportation:
-
+my_list = ["dog", "cat", "bird", "frog", "fish"]
 transportation_means = ["plane", "car", "ship", "bicycle"]
-
+print(transportation_means.append("motorcycle"))
+print(transportation_means)
+print(transportation_means.remove("motorcycle"))
+print(transportation_means)
+newTrans = transportation_means.pop()
+print("My new trans is "+ newTrans  )
 # You must not modify the already supplied line of code, but must use the appropriate list method to add a new element.
 
 # here are two lists:
